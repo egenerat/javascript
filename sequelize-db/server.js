01751a78b0f84ef616cad5a919d62f7150511928
egenerat <emile.generat@outlook.com>
2017-11-03 20:11:30 +0000

    Example with comparison between create and findOrCreate

--- sequelize-db/server.js
+++ sequelize-db/server.js
@@ -14,3 +14,32 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database: ', err);
   });
+
+const Post = sequelize.define('post', {
+  title: Sequelize.STRING,
+  body: Sequelize.TEXT
+})
+  
+sequelize.sync()
+  .then(() => Post.create({
+    title: 'Multiple',
+    body: 'This object is saved many times in db'
+  }))
+  .then(jane => {
+    console.log(jane.get({
+      plain: true
+    }));
+  });
+  
+Post
+  .findOrCreate({where: {
+    title: 'Unique'
+  }, defaults: {
+    body: 'This one is saved only once'}
+  })
+  .spread((user, created) => {
+    console.log(user.get({
+      plain: true
+    }))
+    console.log("Was created: " + created)
+  });
